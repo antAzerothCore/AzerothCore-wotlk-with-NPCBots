f7f45d34005a1226379ac49f4a7f00e9ab165cbc
atrapalis <aristrapalis@gmail.com>
2024-01-13 21:52:18 +0000

    hack(Core/AI): Fixed a rare crash with bots using vehicle skills

--- src/server/game/AI/NpcBots/bot_ai.cpp
+++ src/server/game/AI/NpcBots/bot_ai.cpp
@@ -15709,11 +15709,12 @@ void bot_ai::OnBotOwnerSpellGo(Spell const* spell, bool ok)
                     veh->GetBase()->SetFacingTo(me->GetAbsoluteAngle(target));
                 }
             }
-
+            //npcbot_plus
             //bug: gameobject damage is not sent to players (not visible in log)
             //tempfix: set bot as original caster
-            Spell* vehspell = new Spell(veh->GetBase(), spellInfo, TRIGGERED_NONE/*, me->GetGUID()*/);
-            vehspell->prepare(&targets);
+            //Spell* vehspell = new Spell(veh->GetBase(), spellInfo, TRIGGERED_NONE/*, me->GetGUID()*/);
+            //vehspell->prepare(&targets);
+            //end npcbot_plus
         }
     }
 }
